77ba95d7d1203a1cfb105bfb2701bbe0ae7a0418
Bislan <mamaliev.bislan@mail.ru>
2022-12-27 14:26:59 +0300

    example2

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -1,15 +1,16 @@
-package com.example.dependencyinjectionstart.example2
+package com.example.dependencyinjectionstart.example2.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.dependencyinjectionstart.R
-import com.example.dependencyinjectionstart.example1.Activity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: ExampleViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val activity = Activity()
-        activity.computer.toString()
+        viewModel.method()
     }
 }
